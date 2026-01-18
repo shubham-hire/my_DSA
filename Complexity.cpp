15c94952e794fa59bc5f0f288f69ff361220e95e
Shubham Hire <shubham.hire2006@gmail.com>
2026-01-18 22:49:05 +0530

    learned time and space complexity

---
+++ Complexity.cpp
@@ -0,0 +1,23 @@
+// In this we will learn about time and space complexity....
+
+// Time complexity
+
+// -> amt of time taken as function of input size n.
+
+// Big O
+// -> worst case scenario/upperbound
+
+// Theta
+// -> average case scenario
+
+// omega
+// -> lowerbound best case scenario
+
+// if func is polynomial
+// then highest degree term in polynomial is time complexity
+
+// Space complexity
+
+// -> amt of space taken by an algorithm as function of input size n.
+
+
