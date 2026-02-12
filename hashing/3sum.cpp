e9ea38a5c75458a0a4301f5c37514f28e2fb6448
Shubham Hire <shubham.hire2006@gmail.com>
2026-02-12 18:48:47 +0530

    Optimization

--- hashing/3sum.cpp
+++ hashing/3sum.cpp
@@ -27,11 +27,40 @@ vector<vector<int>> threeSum(vector<int> &nums) {
 
   return ans;
 }
+
+
+// Optimized approach
+vector<vector<int>> threeSum2(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> uniqueTriplet; // set<uniqueTriplets>
+
+        for(int i=0; i<n; i++){
+            int tar = -nums[i];
+            set<int> s;
+
+            for(int j=i+1; j<n; j++){
+                int third = tar - nums[j];
+
+                if(s.find(third) != s.end()){
+                    vector<int> trip = {nums[i], nums[j], third};
+                    sort(trip.begin(), trip.end());
+                    uniqueTriplet.insert(trip);
+
+                }
+                s.insert(nums[j]);
+            }
+        }
+        vector<vector<int>> ans(uniqueTriplet.begin(), uniqueTriplet.end());
+        return ans;
+
+    }
+
+
 int main() {
   vector<int> nums = {-1, 0, 1, 2, -1, -4};
   vector<vector<int>> ans = threeSum(nums);
 
-  for (auto val : ans) {
+  for (vector<int> val : ans) {
     for (auto val2 : val) {
       cout << val2 << ' ';
     }
